1a0c33db0031f7c84bf7bd6d21d167344c3ff0f0
sasha sims <solely.sashasims@gmail.com>
2022-11-05 20:49:25 -0400

    preliminary completed

---
+++ main.js
@@ -0,0 +1,21 @@
+/*      Preliminary
+   
+1. Write a for loop that prints to the console the numbers 0 through 9.
+
+*/
+for(var i=0; i<10; i++){
+    console.log(i)
+}
+
+// 2. Write a for loop that prints to the console 9 through 0.
+
+for(var i=9; i>=0; i--){
+    console.log(i)
+}
+
+// 3. Write a for loop that prints these fruits to the console.const fruit = ["banana", "orange", "apple", "kiwi"]
+
+const fruit = ["banana", "orange", "apple", "kiwi"]
+for(var i=0; i<fruit.length; i++){
+    console.log(fruit[i])
+}
