6d0021ee34cc6dfad86e683ca6f17be5216b5715
sasha sims <solely.sashasims@gmail.com>
2022-11-05 21:32:40 -0400

    bronze medal achieved

--- main.js
+++ main.js
@@ -19,3 +19,34 @@ const fruit = ["banana", "orange", "apple", "kiwi"]
 for(var i=0; i<fruit.length; i++){
     console.log(fruit[i])
 }
+
+//Bronze Medal
+
+//1. Write a for loop that will push the numbers 0 through 9 to an array.
+
+const numArray = []
+for(i=0; i<10; i++){
+numArray.push(i)
+}
+
+console.log(numArray)
+// 2. Write a for loop that prints to the console only even numbers 0 through 100.
+
+for(i=0; i<101; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
+
+// 3. Write a for loop that will push every other fruit to an array.`const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]`
+
+const fruit2 = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+const fruit3 = [];
+for(var i=0; i<fruit2.length; i++){
+  if(i % 2 === 0){
+    fruit3.push(fruit2[i])
+   
+  }
+}
+console.log(fruit3)
